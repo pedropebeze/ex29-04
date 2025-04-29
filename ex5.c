0f7f7aa0bd5d99f6473a52fa47b879ff67350972
pedropebeze <pedropassareli.b@gmail.com>
2025-04-29 20:27:17 -0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -2,6 +2,7 @@
 
     double media(double n[]);
     double dobro(double n[]);
+    double maior(double n[]);
 
 int main() {
     
@@ -10,16 +11,31 @@ int main() {
     scanf("%lf",&numeros[0]);
     puts("Digite um numero: ");
     scanf("%lf",&numeros[1]);
-    printf("Media: %.2lf\n",media(numeros));
-
-
     
+    printf("\nVetor antes: %.0lf, %.0lf\n",numeros[0],numeros[1]);
+    maior(numeros);
+    printf("Vetor ordenado: %.0lf, %.0lf\n",numeros[0],numeros[1]);
+    printf("\nMedia: %.1lf\n",media(numeros));
+    printf("Dobro: %.1lf\n",dobro(numeros));
+
+    printf("\nVetor depois: %.0lf, %.0lf\n",numeros[0],numeros[1]);
+
     return 0;
 }
 
+    double maior(double n[]){
+        if(n[0]<n[1]){
+            float temp = n[0];
+            n[0] = n[1];
+            n[1] = temp;
+        }
+
+    }
     double media(double n[]){
         return (n[0]+n[1])/2;
     }
     double dobro(double n[]){
-        
+        n[0]*=2;
+        n[1]*=2;
+        return (n[0]+n[1])/2;
     }
